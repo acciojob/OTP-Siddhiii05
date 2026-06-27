19ed8fe72f836854d9fc994eaf9f9dc01317290b
Siddhiii05 <siddhi.bobade05@gmail.com>
2026-06-27 18:05:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, index) => {
+  code.addEventListener("input", (e) => {
+    e.target.value = e.target.value.slice(-1);
+
+    if (e.target.value !== "" && index < codes.length - 1) {
+      codes[index + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (code.value === "" && index > 0) {
+        codes[index - 1].value = "";
+        codes[index - 1].focus();
+      }
+    }
+  });
+});
